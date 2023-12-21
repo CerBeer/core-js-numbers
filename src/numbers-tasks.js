bc740863f0f826c82c2938d567ed17d12f267654
CerBeer <ocelopp@gmail.com>
2023-12-21 10:15:54 +0300

    feat: implements from start to getCube

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -159,8 +159,9 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  const d = a * a + b * b;
+  return Math.sqrt(c * c + d);
 }
 
 /**
@@ -180,8 +181,8 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / 10 ** pow) * 10 ** pow;
 }
 
 /**
@@ -201,8 +202,11 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) return false;
+  const sq = Math.sqrt(n);
+  for (let i = 2; i <= sq; i += 1) if (n % i === 0) return false;
+  return true;
 }
 
 /**
@@ -220,8 +224,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number.parseInt(`${value}`, 10) || def;
 }
 
 /**
@@ -235,8 +239,8 @@ function toNumber(/* value, def */) {
  *   -2 => -8
  *   0  => 0
  */
-function getCube(/* num */) {
-  throw new Error('Not implemented');
+function getCube(num) {
+  return num ** 3;
 }
 
 /**
